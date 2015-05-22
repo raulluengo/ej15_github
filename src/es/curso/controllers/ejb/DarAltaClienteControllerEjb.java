72141b8d7646fa330c2e921f35e0c8587793fdfc
MAÑANA <MAANA@aula2-PC12>
2015-05-22 11:57:12 +0200

    ASS DraAltaClienteController >  method

---
+++ src/es/curso/controllers/ejb/DarAltaClienteControllerEjb.java
@@ -0,0 +1,18 @@
+package es.curso.controllers.ejb;
+
+import es.curso.controllers.DarAltaClienteController;
+import es.curso.model.Entity.Cliente;
+
+public class DarAltaClienteControllerEjb implements DarAltaClienteController {
+
+	@Override
+	public void agregar(Cliente cliente) {
+		// Logica del negocio para agregar un cliente
+		// Verificar datos, agregarlos (llamar a la capa dao), enviar email etc..
+	
+		
+	}
+	
+	
+
+}
